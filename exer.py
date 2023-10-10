b70a3ae22cad66fb53a5b7d50da42c4988da0b70
rosegithub-dotcom <143061942+rosegithub-dotcom@users.noreply.github.com>
2023-10-10 14:00:04 +0300

    MUBEZI ROSE NAUME

---
+++ exer.py
@@ -0,0 +1,27 @@
+#whats the python programe to sum all the numbers in a list[8,2,3,0,7]
+def sum_of_numbers_in_list(s,d,f,g,h):
+  answer=[s+d+f+g+h]
+  print(answer) 
+sum_of_numbers_in_list(8,2,3,0,7)
+
+
+#write a python function to multiply all numbers in alist[8,2,3,-1,7]
+def product_of_all_list_numbers(a,b,c,d,e):
+    output=[a*b*c*d*e]
+    print(output)
+product_of_all_list_numbers(8,2,3,-1,7)
+
+#write a python function to reverse a string "1234abcd"
+def my_string(str):
+    str="1234abcd"
+    reversed_string=(str)
+    for i in reversed(str):
+       print(i)
+my_string("1234abcd")
+
+
+    #write a python programe to print the even numbers in a given list[1,2,3,4,5,6,7,8,9]
+def even_numbers_in_list(a,b,c,d,e,f,g,j,k):
+    even_list=[b,d,f,j]
+    print(even_list)
+even_numbers_in_list(1,2,3,4,5,6,7,8,9)
